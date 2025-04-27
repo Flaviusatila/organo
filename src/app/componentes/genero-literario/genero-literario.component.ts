455cd52677b5e76fff59606aa710c6473ead8a13
Flavius Atila <flaviusatila@gmail.com>
2025-04-27 18:15:44 -0300

    Add genre-specific styles to GeneroLiterario and Livro components, and update HTML to apply dynamic classes based on genre

--- src/app/componentes/genero-literario/genero-literario.component.ts
+++ src/app/componentes/genero-literario/genero-literario.component.ts
@@ -1,10 +1,14 @@
 import { Component, input } from '@angular/core';
 import { GeneroLiterario } from '../livro/livro';
 import { LivroComponent } from '../livro/livro.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-genero-literario',
-  imports:[LivroComponent],
+  imports:[
+    LivroComponent,
+    CommonModule
+  ],
   templateUrl: './genero-literario.component.html',
   styleUrls: ['./genero-literario.component.css']
 })
