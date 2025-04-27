4061bedd845848c8bbc39299faa38b4a30171f59
Flavius Atila <flaviusatila@gmail.com>
2025-04-27 18:01:30 -0300

    Replace LivroComponent with ListaLivrosComponent in app layout, and add mock data for books in mock-livros.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { GeneroLiterarioComponent } from './componentes/genero-literario/genero-literario.component';
+import { ListaLivrosComponent } from './componentes/lista-livros/lista-livros.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
-import { LivroComponent } from "./componentes/livro/livro.component";
 
 @Component({
   selector: 'app-root',
-  imports: [CabecalhoComponent, RodapeComponent, LivroComponent],
+  imports: [
+    CabecalhoComponent,
+    RodapeComponent,
+    GeneroLiterarioComponent,
+    ListaLivrosComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
